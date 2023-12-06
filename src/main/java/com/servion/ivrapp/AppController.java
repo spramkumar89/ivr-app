4aaa56b321fab82eab1814833daff5bf75ecb1a1
Ramkumar <spramkumar89@gmail.com>
2023-12-06 10:11:47 +0800

    Testing service

--- src/main/java/com/servion/ivrapp/AppController.java
+++ src/main/java/com/servion/ivrapp/AppController.java
@@ -14,5 +14,6 @@ public class AppController {
         model.setViewName(page);
         return model;
     }
+    
 }
 
---
+++ src/main/java/com/servion/ivrapp/MetricsController.java
@@ -0,0 +1,18 @@
+package com.servion.ivrapp;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MetricsController {
+
+	@GetMapping("/ivr/metrics")
+    public String metrics(){
+		System.out.println("App Version : To check the rolling update");
+        String str = "App Version : To check the rolling update";
+        str = str + " : " + new Date();
+        return str;
+    }
+}
